96b496de7fc8980a1765bf8f54fd8c910b09a584
Salieri-ino <tahitahi31@gmail.com>
2021-03-29 21:55:59 +0900

    ユーザープロフィール画面の作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('create/', views.UserCreateView.as_view(), name="create"),
+    path('profile/', views.UserProfileView.as_view(), name="profile"),
 ]
