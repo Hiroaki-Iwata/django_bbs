42430f00643e958a350dae0d96d7e8b8e4704d76
Salieri-ino <tahitahi31@gmail.com>
2021-03-30 00:04:54 +0900

    ユーザー情報変更画面

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
+from .forms import UserChangeForm
 
 class UserCreateView(FormView):
     form_class = UserCreationForm
@@ -34,3 +35,23 @@ class UserProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'registration/profile.html'
     def get_queryset(self):
         return User.objects.get(id=self.request.user.id)
+
+class UserChangeView(LoginRequiredMixin, FormView):
+    template_name = 'registration/change.html'
+    form_class = UserChangeForm
+    success_url = reverse_lazy('accounts:profile')
+
+    def form_valid(self, form):
+        #formのupdateメソッドにログインユーザーを渡して更新
+        form.update(user=self.request.user)
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        # 更新前のユーザー情報をkwargsとして渡す
+        kwargs.update({
+            'email' : self.request.user.email,
+            'first_name' : self.request.user.first_name,
+            'last_name' : self.request.user.last_name,
+        })
+        return kwargs
